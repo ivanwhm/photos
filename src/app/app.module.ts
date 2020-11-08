ed2318eb1320a0d813d67300c2473075bcfab23a
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-08 12:51:29 -0300

    API Signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,7 +7,7 @@ import { PhotoShowComponent } from './photo-show/photo-show.component';
 
 @NgModule({
   declarations: [AppComponent, PhotoShowComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
