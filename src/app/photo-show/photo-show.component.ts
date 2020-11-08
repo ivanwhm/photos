110c637404bf3045396070effdc776cdb7d4810d
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-08 13:08:38 -0300

    Displaying the Component

--- src/app/photo-show/photo-show.component.ts
+++ src/app/photo-show/photo-show.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PhotosService } from '../photos.service';
+
 @Component({
   selector: 'app-photo-show',
   templateUrl: './photo-show.component.html',
   styleUrls: ['./photo-show.component.css'],
 })
 export class PhotoShowComponent implements OnInit {
-  constructor() {}
+  constructor(private service: PhotosService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getPhoto();
+  }
+
+  getPhoto(): any {
+    this.service.getPhoto().subscribe((value) => console.log(value));
+  }
 }
