2739686f9d5b4591f40de93915239b80b648968a
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-08 13:04:42 -0300

    Making the Request

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,9 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const API_URL = 'https://api.unsplash.com';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotosService {
   constructor(private http: HttpClient) {}
+
+  getPhoto(): Observable<any> {
+    return this.http.get(`${API_URL}/photos/random`, {
+      headers: {
+        Authorization: 'Client-ID L6iJKjKHWCd4FgQqClNNcp01gmc9MG6MOXgjXJ6mqDY',
+      },
+    });
+  }
 }
