916aefcac1a5375de1704df0d14ba433dcf10c66
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-08 12:47:30 -0300

    App Architecture Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PhotoShowComponent } from './photo-show/photo-show.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PhotoShowComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
